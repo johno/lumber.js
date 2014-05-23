4c358be6c7608d12387589f46ac57385f9475459
John Otander <johnotander@gmail.com>
2014-05-23 15:24:20 -0600

    Add using.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var sass   = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var using  = require('gulp-using');
 
 gulp.task('lint', function() {
   return gulp.src('lumber.js')
@@ -15,17 +16,20 @@ gulp.task('lint', function() {
 gulp.task('sass', function() {
   return gulp.src('scss/*.scss')
     .pipe(sass())
+    .pipe(using({ color: 'blue' }))
     .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('dist-sass', function() {
   return gulp.src('scss/*.scss')
+    .pipe(using({ color: 'blue' }))
     .pipe(gulp.dest('dist/scss'));
 });
 
 gulp.task('scripts', function() {
   return gulp.src('lumber.js')
     .pipe(gulp.dest('dist'))
+    .pipe(using({ color: 'blue' }))
     .pipe(rename('lumber.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
