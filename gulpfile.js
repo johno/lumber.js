1abe89e6457331de7a413ef712352f343064d10e
John Otander <johnotander@gmail.com>
2014-05-23 15:00:09 -0600

    Add gulpfile with tasks.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var sass   = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('lint', function() {
+  return gulp.src('lumber.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('lumber.js')
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('lumber.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('lumber.js', ['lint', 'scripts']);
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
